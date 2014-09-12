1d5585ca3225c9a79cb82e31e2071daff8cce9ef
Giustino Borzacchiello <giustinob@gmail.com>
2014-09-12 23:03:32 +0200

    Added product creation

--- src/Acme/StoreBundle/Controller/DefaultController.php
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -2,12 +2,27 @@
 
 namespace Acme\StoreBundle\Controller;
 
+use Acme\StoreBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('AcmeStoreBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('Ride a Foo');
+        $product->setPrice('19.99');
+        $product->setDescription('Lorem ipsum dolor sit amet');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+
+        return new Response('Created product id ' . $product->getId());
     }
 }
