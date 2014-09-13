eefcbefefae23a3895ff841596da1b7b5f2078a1
Giustino Borzacchiello <giustinob@gmail.com>
2014-09-13 09:16:53 +0200

    Created show action for products

--- src/Acme/StoreBundle/Controller/DefaultController.php
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -15,14 +15,23 @@ class DefaultController extends Controller
     public function createAction()
     {
         $product = new Product();
-        $product->setName('Ride a Foo');
-        $product->setPrice('19.99');
-        $product->setDescription('Lorem ipsum dolor sit amet');
+        $product->setName('Stampo per dolci');
+        $product->setPrice('34.10');
+        $product->setDescription('Uno stampo per dolci grande quanto una casa');
 
         $em = $this->getDoctrine()->getManager();
         $em->persist($product);
         $em->flush();
 
         return new Response('Created product id ' . $product->getId());
+    }
+
+    public function showAction($id)
+    {
+        $product = $this->getDoctrine()->getRepository('AcmeStoreBundle:Product')->find($id);
+        if(!$product) {
+            throw $this->createNotFoundException('No product for id ' . $id);
+        }
+        return $this->render('AcmeStoreBundle:Default:show.html.twig', array('product' => $product));
     }
 }
